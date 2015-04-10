276d3000d9b97ca0c0b8a492842d8d94f25d1b88
Joe Abhishek <joeabhishek@gmail.com>
2015-04-10 16:37:06 -0400

    Create a messaging service using MYO and Glass

---
+++ app/src/main/java/com/thalmic/android/myoglass/ConfigActivity.java
@@ -0,0 +1,551 @@
+package com.thalmic.android.myoglass;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.glass.companion.Proto;
+import com.thalmic.android.myoglass.bluetoothchat.BluetoothChatService;
+import com.thalmic.android.myoglass.bluetoothchat.Constants;
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.Arm;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.XDirection;
+import com.thalmic.myo.scanner.ScanActivity;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Set;
+
+public class ConfigActivity extends Activity implements GlassDevice.GlassConnectionListener {
+    private static final String TAG = "ConfigActivity";
+
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    private MyoRemoteService mService;
+    private StopReceiver mStopReceiver = new StopReceiver();
+
+    private AppPrefs mPrefs;
+    private DeviceListener mListener;
+
+    private TextView mMyoStatusView;
+    private TextView mGlassStatusView;
+    private ImageView mScreencastView;
+    private Button mScreencastButton;
+    private TextView mArmView;
+    private TextView mPoseView;
+
+    private GlassDevice mGlass;
+    private boolean mScreencastEnabled = false;
+
+
+    //Bluetooth Chat
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    //private static final int REQUEST_ENABLE_BT = 3;
+
+    // Layout Views
+    private ListView mConversationView;
+    private EditText mOutEditText;
+    private Button mSendButton;
+
+    /**
+     * Name of the connected device
+     */
+    private String mConnectedDeviceName = null;
+
+    /**
+     * Array adapter for the conversation thread
+     */
+    private ArrayAdapter<String> mConversationArrayAdapter;
+
+    /**
+     * String buffer for outgoing messages
+     */
+    private StringBuffer mOutStringBuffer;
+
+    /**
+     * Local Bluetooth adapter
+     */
+    private BluetoothAdapter mBluetoothAdapter = null;
+
+    /**
+     * Member object for the chat services
+     */
+    private BluetoothChatService mChatService = null;
+
+    private Activity mCurrentActivity = null;
+    public Activity getCurrentActivity(){
+        return mCurrentActivity;
+    }
+    public void setCurrentActivity(Activity mCurrentActivity){
+        this.mCurrentActivity = mCurrentActivity;
+    }
+
+    //BLUETOOTH Chat End
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyoRemoteService.MBinder mbinder = ((MyoRemoteService.MBinder)service);
+            mService = mbinder.getService();
+
+            if (mListener == null) {
+                mListener = new MyoListener();
+                Hub.getInstance().addListener(mListener);
+            }
+
+            mGlass = mService.getMyoRemote().getGlassDevice();
+            mGlass.registerListener(ConfigActivity.this);
+            updateGlassStatus(mGlass.getConnectionStatus());
+
+            String glassAddress = mPrefs.getGlassAddress();
+            if (!TextUtils.isEmpty(glassAddress)) {
+                mService.getMyoRemote().connectGlassDevice(glassAddress);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_config);
+        mMyoStatusView = (TextView) findViewById(R.id.myo_status);
+        mGlassStatusView = (TextView) findViewById(R.id.glass_status);
+        mScreencastView = (ImageView) findViewById(R.id.screenshot);
+        mScreencastButton = (Button) findViewById(R.id.btnStartScreencast);
+        mPoseView = (TextView) findViewById(R.id.pose);
+        mArmView = (TextView) findViewById(R.id.arm);
+
+        //Bluetooth Chat
+        mConversationView = (ListView) findViewById(R.id.in);
+        mOutEditText = (EditText) findViewById(R.id.edit_text_out);
+        mSendButton = (Button) findViewById(R.id.button_send);
+        // End
+
+        updateScreencastState();
+
+        mPrefs = new AppPrefs(this);
+
+        Intent intent = new Intent(this, MyoRemoteService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+
+        registerReceiver(mStopReceiver, new IntentFilter(MyoRemoteService.ACTION_STOP_MYO_GLASS));
+
+        //Bluetooth Chat
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            this.finish();
+        }
+
+        setCurrentActivity(this);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        } else if (mChatService == null) {
+            setupChat();
+            //sendMessage("Hello");
+        }
+
+
+    }
+
+    private void setupChat() {
+        Log.d(TAG, "setupChat()");
+
+        // Initialize the array adapter for the conversation thread
+        mConversationArrayAdapter = new ArrayAdapter<String>(this, R.layout.message);
+
+        mConversationView.setAdapter(mConversationArrayAdapter);
+
+        // Initialize the compose field with a listener for the return key
+        mOutEditText.setOnEditorActionListener(mWriteListener);
+
+        // Initialize the send button with a listener that for click events
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Send a message using content of the edit text widget
+                View view = getWindow().getDecorView().findViewById(android.R.id.content);
+                if (null != view) {
+                    TextView textView = (TextView) view.findViewById(R.id.edit_text_out);
+                    String message = textView.getText().toString();
+                    sendMessage(message);
+                }
+            }
+        });
+
+        // Initialize the BluetoothChatService to perform bluetooth connections
+        mChatService = new BluetoothChatService(this, mHandler);
+        mChatService.start();
+
+        // Initialize the buffer for outgoing messages
+        mOutStringBuffer = new StringBuffer("");
+    }
+
+    /**
+     * The action listener for the EditText widget, to listen for the return key
+     */
+    private TextView.OnEditorActionListener mWriteListener
+            = new TextView.OnEditorActionListener() {
+        public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+            // If the action is a key-up event on the return key, send the message
+            if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
+                String message = view.getText().toString();
+                sendMessage(message);
+            }
+            return true;
+        }
+    };
+
+    /**
+     * Makes this device discoverable.
+     */
+    private void ensureDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+
+    /**
+     * Sends a message.
+     *
+     * @param message A string of text to send.
+     */
+    private void sendMessage(String message) {
+        // Check that we're actually connected before trying anything
+        if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
+            Toast.makeText(this, "Not Connected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            mChatService.write(send);
+
+            // Reset out string buffer to zero and clear the edit text field
+            mOutStringBuffer.setLength(0);
+            mOutEditText.setText(mOutStringBuffer);
+        }
+    }
+
+    /**
+     * The Handler that gets information back from the BluetoothChatService
+     */
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Activity activity = getCurrentActivity();
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            //setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
+                            mConversationArrayAdapter.clear();
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            //setStatus(R.string.title_connecting);
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            //setStatus(R.string.title_not_connected);
+                            break;
+                    }
+                    break;
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    mConversationArrayAdapter.add("Me:  " + writeMessage);
+                    break;
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    if(readMessage.equals("STOP")) {
+                        mChatService.stop();
+                    }
+                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    if (null != this) {
+                        Toast.makeText(activity, "Connected to "
+                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case Constants.MESSAGE_TOAST:
+                    if (null != activity) {
+                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    };
+
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Hub.getInstance().removeListener(mListener);
+        unregisterReceiver(mStopReceiver);
+        unbindService(mServiceConnection);
+        mGlass.unregisterListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGlass.stopScreenshot();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!BluetoothAdapter.getDefaultAdapter().isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.config, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.kill_myglass:
+                killMyGlass();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void killMyGlass() {
+        Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setData(Uri.parse("package:com.google.glass.companion"));
+        startActivity(intent);
+    }
+
+    public void onScreencastBtn(View view) {
+        setScreencastEnabled(!mScreencastEnabled);
+        if (mScreencastEnabled){
+            mGlass.requestScreenshot();
+        } else{
+            mGlass.stopScreenshot();
+        }
+    }
+
+    private void setScreencastEnabled(boolean enable) {
+        mScreencastEnabled = enable;
+        updateScreencastState();
+    }
+
+    private void updateScreencastState(){
+        mScreencastButton.setText(mScreencastEnabled ? R.string.stop : R.string.start);
+        mScreencastView.setVisibility(mScreencastEnabled ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    private void updateGlassStatus(GlassDevice.ConnectionStatus connectionStatus) {
+        mGlassStatusView.setText(connectionStatus.name());
+    }
+
+    public void onChooseMyoClicked(View view) {
+        if (mService == null) {
+            Log.w(TAG, "No MyoRemoveService. Can't choose Myo.");
+            return;
+        }
+
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
+    }
+
+    public void onChooseGlassClicked(View view) {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if (pairedDevices == null || pairedDevices.isEmpty()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.no_devices_title)
+                    .setMessage(R.string.no_devices_message)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+            return;
+        }
+
+        String[] glassNames = new String[pairedDevices.size()];
+        final String[] glassAddresses = new String[pairedDevices.size()];
+        int i = 0;
+        for (BluetoothDevice device : pairedDevices) {
+            glassNames[i] = device.getName();
+            glassAddresses[i] = device.getAddress();
+            i++;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.choose_glass);
+        builder.setItems(glassNames, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (mGlass != null && mGlass.getConnectionStatus() == GlassDevice.ConnectionStatus.CONNECTED) {
+                    mService.getMyoRemote().closeGlassDevice();
+                }
+                mService.getMyoRemote().connectGlassDevice(glassAddresses[which]);
+
+                // Remember MAC address for next time.
+                mPrefs.setGlassAddress(glassAddresses[which]);
+            }
+        });
+        builder.show();
+    }
+
+    private class StopReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            finish();
+        }
+    }
+
+    private class MyoListener extends AbstractDeviceListener {
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            mPrefs.setMyoAddress(myo.getMacAddress());
+            mMyoStatusView.setText(R.string.connected);
+            mPoseView.setText("LOCKED");
+        }
+
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            mMyoStatusView.setText(R.string.disconnected);
+            mArmView.setText("");
+            mPoseView.setText("");
+        }
+
+        @Override
+        public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
+            mArmView.setText(arm == Arm.LEFT ? R.string.myo_arm_left : R.string.myo_arm_right);
+        }
+
+        @Override
+        public void onArmUnsync(Myo myo, long timestamp) {
+            mArmView.setText(R.string.myo_arm_unknown);
+        }
+
+        @Override
+        public void onPose(Myo myo, long timestamp, final Pose pose) {
+            mPoseView.setText(pose.name());
+        }
+
+        @Override
+        public void onLock(Myo myo, long timestamp) {
+            mPoseView.setText("LOCKED");
+        }
+
+        @Override
+        public void onUnlock(Myo myo, long timestamp) {
+            mPoseView.setText("UNLOCKED");
+        }
+    }
+
+    @Override
+    public void onConnectionStatusChanged(GlassDevice.ConnectionStatus status) {
+        updateGlassStatus(status);
+        if (status != GlassDevice.ConnectionStatus.DISCONNECTED) {
+            setScreencastEnabled(false);
+        }
+    }
+
+    // Called when a message from Glass is received
+    public void onReceivedEnvelope(Proto.Envelope envelope){
+        if (envelope.screenshot != null) {
+            if (envelope.screenshot.screenshotBytesG2C != null) {
+                InputStream in = new ByteArrayInputStream(envelope.screenshot.screenshotBytesG2C);
+                final Bitmap bp = BitmapFactory.decodeStream(in);
+
+                // Update the UI
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mScreencastView.setImageBitmap(bp);
+                    }
+                });
+            }
+        }
+    }
+}
